1bceac0863622d27879087cfd0cd7db4a5279806
Matheus Heck <matheus@conduite.tv>
2021-01-06 15:18:39 +0100

    noHyperdrive on end

--- src/modules/starships/services/ListStarshipsRankedService.py
+++ src/modules/starships/services/ListStarshipsRankedService.py
@@ -5,10 +5,17 @@ from ....shared.infra.sqlalchemy.base import Session
 
 def execute():
     session = Session()
-
     spaceships = session.query(Ship).all()
-    orderedShips = sorted(spaceships, key=lambda x: x.hyperdrivespeed, reverse=False)
+    noHyperdrive = []
 
+    for spaceship in spaceships:
+        if spaceship.hyperdrivespeed < 0:
+            noHyperdrive.insert(1, spaceship)
+            spaceships.remove(spaceship)
+
+    
+    orderedShips = sorted(spaceships, key=lambda x: x.hyperdrivespeed, reverse=False)
+    orderedShips.extend(noHyperdrive)
 
     session.close()
     return orderedShips
