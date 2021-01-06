d383cabc3c5601157446f574f1ab30ef367c662b
Matheus Heck <matheus@conduite.tv>
2021-01-06 15:30:36 +0100

    front cli

--- __main__.py
+++ __main__.py
@@ -5,25 +5,25 @@ import src.modules.starships.services.ListStarshipsRankedService as ListStarship
 import src.modules.starships.services.CreateStarshipsTableService as CreateStarshipsTableService
 
 def rank():
-    print ('Welcome to Starships of War!')
+    print ('\n \n### Welcome to Starships of War! ###\n \n')
 
     CreateStarshipsTableService.execute()
     spaceships = ListStarshipsRankedService.execute()
 
     if not spaceships:
-        print('\n### Filling database...')
+        print('\n### Filling database...\n')
         InsertStarshipsService.execute()
         spaceships = ListStarshipsRankedService.execute()
 
-    print('\n### All spaceships, ranked by Hyperdriver class:')
+    print('\n### All spaceships, ranked by Hyperdriver class: ###\n')
 
     for spaceship in spaceships:
         if spaceship.hyperdrivespeed > 0:
-            print(f'{spaceship.name} has a {spaceship.hyperdrivespeed} hyperdriver class and a {spaceship.sublightspeed}000 sublight speed.')
+            print(f'# {spaceship.name} has a {spaceship.hyperdrivespeed} hyperdriver class and a {spaceship.sublightspeed}000 sublight speed.')
             print('')
 
         else:
-            print(f'{spaceship.name} has no hyperdriver class but a {spaceship.sublightspeed}000 sublight speed.')
+            print(f'# {spaceship.name} has no hyperdriver class but a {spaceship.sublightspeed}000 sublight speed.')
             print('')
 
 if __name__ == '__main__':
